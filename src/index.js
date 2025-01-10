a87ef3d079185b6dbf8e8b6c50b6bebd8d294d9c
Faizan Saifi <faizanwork112@gmail.com>
2025-01-10 21:09:12 +0530

    user and videoschema update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
 dotenv.config({path:'./env'})
 
 
 
-import express from "express";
-const app = express();
-connectDB();
+
+
+connectDB()
+
+.then(()=>{
+    app.listen(process.env.PORT ||8000,()=>{
+console.log(`server is running at port http://localhost${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("Mongo db connection failed !!!", err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // ;(async()=>{
 //     try{
